559eb6421143181484cc3e6d2f4ade3b60d3eeba
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-15 16:55:57 -0600

    commit tree 89b671122ff4d8c87496cbfacddb454acabc8ca7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState }from "react";
 
 function Item({ name, category }) {
+  
+  const [isOn1, setIsOn1] = useState(false)
+  
+  function handleClcik(){
+    setIsOn1((isOn1) => !isOn1)
+  }
+
+const itemInCart = isOn1 ? "Remove From Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={isOn1 ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isOn1 ? "remove" : "add"} onClick={handleClcik}>{itemInCart}</button>
     </li>
   );
 }
